3eb402a56b8821d7b91c170a9a2a146a123fb347
Renato Alushi <renato.alushi@raiffeisen.al>
2021-12-08 20:44:07 +0100

    Basket BLL

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCard> GetBasket(string userName);
+        Task<ShoppingCard> UpdateBasket(ShoppingCard basket);
+        Task DeleteBasket(string userName);
+    }
+}
