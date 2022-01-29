0f2afb191044a86d6ce497f45e4fd98c31e2dea3
Renato Alushi <renato.alushi@raiffeisen.al>
2022-01-30 00:08:24 +0100

    finish implement of order Api with clean architecture

--- src/Services/Ordering/Ordering.Infastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ordering.Infastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Services/Ordering/Ordering.Infastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infastructure.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                    .Where(o => o.UserName == userName)
+                                    .ToListAsync();
+            return orderList;
+        }
+    }
+}
