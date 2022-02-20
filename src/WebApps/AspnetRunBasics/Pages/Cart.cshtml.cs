6a598b196adebdf2f833910b97e02cb0d96ae0e4
Renato Alushi <renato.alushi@raiffeisen.al>
2022-02-20 17:43:17 +0100

    Implement Web page for e-comerse

---
+++ src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+
+        public CartModel(IBasketService cartRepository)
+        {
+            _basketService = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+        }
+
+        public BasketModel Cart { get; set; } = new BasketModel();
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userName = "test";
+            Cart = await _basketService.GetBasket(userName);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            var userName = "test";
+            var basket = await _basketService.GetBasket(userName);
+
+            var item = basket.Items.Single(x => x.ProductId == productId);
+            basket.Items.Remove(item);
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
+            return RedirectToPage();
+        }
+    }
+}
